376ff0454755150aaddff618112636ef24a3895e
Diederik <d.p.siderius@student.tudelft.nl>
2024-01-08 10:38:13 +0100

    random goal

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ obstacles = np.array(obstacles)
 
 # Define the start and goal positions and step size and max iterations
 start = np.array([0, 0, 0.25 + 0.5])
-goal = np.array([2, 5.5, 0.7])
+goal = np.array([np.random.uniform(-1, 3), np.random.uniform(4.5, 6), np.random.uniform(0.2, 1.2)])
 step_size = 0.1
 max_iter = 1000
 
