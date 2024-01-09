2246d42890581c7f3c5658c41152a67c8d6930fa
Maarten Kokshoorn <maartenkokshoorn@gmail.com>
2024-01-09 11:15:37 +0000

    Metrics.py changed

--- Metrics/metrics.py
+++ Metrics/metrics.py
@@ -3,48 +3,60 @@ import pybullet as p
 import pybullet_data
 import time
 import os
-import matplotlib.pyplot as plt
+import cvxpy as cp
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from rrt_star import plot_rrt
+# Now, import the module using the absolute path
+from RRT_star.class_rrt import plot_rrt
+from RRT_star.class_rrt import RRTStar as rrt
 
+# Load the functions defined in other scripts
+# from RRT_star.class_rrt import rrt_star
+from control_scripts.add_obstacles import add_obstacles
+
+# Initialize the simulation
+p.connect(p.DIRECT)
+p.resetSimulation()
+p.setRealTimeSimulation(0)
+p.setGravity(0, 0, -9.81)
+p.loadURDF(os.path.join(pybullet_data.getDataPath(), "plane.urdf"), [0, 0, 0])
+        
 # Create a dictionary that sets the variables for three different parameter sets. Variables are: step_size, max_iter, num_iterations
 def variable_parameters():
     parameter_set = {
-        "parameter_set_1": [0.1, 500, 5],
-        "parameter_set_2": [0.1, 1000, 5],
-        "parameter_set_3": [0.1, 1500, 5]
+        "parameter_set_1": [0.3, 500, 5],
+        # "parameter_set_2": [0.1, 10000, 1],
+        # "parameter_set_3": [0.1, 50000, 1]
     }
     return parameter_set
 
-def statistics(nodes_per_ideal_path, num_iterations, total_nodes, success_percentage, total_time, time_per_iteration, nodes_per_iteration, success_percentage_per_iteration, path_lengths): 
-    # Compute statistics
-    if len(path_lengths) > 0:
-        mean_path_length = np.mean(path_lengths)
-        std_path_length = np.std(path_lengths)
-    else:
-        mean_path_length = 0.0
-        std_path_length = 0.0
+# Add obstacles to the simulation and list them in a list
+obstacles, _ = add_obstacles()
+obstacles = np.array(obstacles)
 
-    mean_nodes = total_nodes / num_iterations
-    mean_success_percentage = (success_percentage / num_iterations) * 100
-    mean_total_time = total_time / num_iterations
-    std_nodes = np.std(nodes_per_iteration)
-    std_success_percentage = np.std(success_percentage_per_iteration) * 100
-    std_total_time = np.std(time_per_iteration)
-     # Calculate mean nodes per ideal path
-    mean_nodes_per_ideal_path = np.mean(nodes_per_ideal_path)
-    return mean_path_length, std_path_length, mean_nodes, mean_success_percentage, mean_total_time, std_nodes, std_success_percentage, std_total_time, mean_nodes_per_ideal_path
+# Define the start and goal positions and step size and max iterations
+start = np.array([0, 0, 0.25 + 0.5])
+# goal = np.array([np.random.uniform(-1, 3), np.random.uniform(4.5, 6), np.random.uniform(0.2, 1.2)])
 
-# Define parameter variables
-start = np.array([0, 0, 0.7])
-goal = np.array([4, 5, 0.7])
-step_size = 0.1     # Adjust as needed
-max_iter = 500      # Adjust as needed
-num_iterations = 10  # Adjust as needed
+# For RRT_star fixed goal is needed to compare the results
+goal = np.array([2, 5.5, 0.2])
+# step_size = 0.1     # Adjust as needed
+# max_iter = 500      # Adjust as needed
+# num_iterations = 10  # Adjust as needed
+
+def calculate_path_length(path):
+    total_length = 0.0
+    for i in range(len(path) - 1):
+        node1 = np.array(path[i])
+        node2 = np.array(path[i + 1])
+        distance = np.linalg.norm(node2 - node1)
+        total_length += distance
+    return total_length
 
 # Run RRT* algorithm and simulation num_iterations times
-def run_simulation_with_rrt_star(num_iterations, step_size, max_iter):
-    # Initialize variables
+def run_simulation_with_rrt_star(start, goal, obstacles, step_size, max_iter, num_iterations):
+    # Initialize variablesa
     total_nodes = 0
     success_percentage = 0
     total_time = 0.0
@@ -55,14 +67,17 @@ def run_simulation_with_rrt_star(num_iterations, step_size, max_iter):
     rrt_graphs = []
     nodes_per_ideal_path = []
 
+    # Run the RRT* algorithm and show the results
+    rrt_inst = rrt(start, goal, obstacles, step_size, max_iter)
+
     for i in range(num_iterations):
         start_time = time.time()  # Start de tijd voor deze iteratie
 
         # Run RRT* algorithm
-        path, tree = run_rrt_star(start, goal, obstacles, step_size, max_iter)
+        path, tree = rrt_inst.rrt_star_algorithm()
 
-        # Store RRT* graph data for visluasation later
-        rrt_graphs.append((path, tree))
+        # # Store RRT* graph data for visluasation later
+        # rrt_graphs.append((path, tree))
 
         # Check if a valid path is found
         if path is not None:
@@ -85,16 +100,32 @@ def run_simulation_with_rrt_star(num_iterations, step_size, max_iter):
         nodes_per_iteration.append(len(tree)) # Calculate nodes for this iteration
         success_percentage_per_iteration.append(1 if path is not None else 0) # Calculate success percentage for this iteration
 
-
         # Reset simulation for next iteration
         p.resetSimulation()
         p.setGravity(0, 0, -9.81)
         p.setRealTimeSimulation(0)
         p.loadURDF(os.path.join(pybullet_data.getDataPath(), "plane.urdf"), [0, 0, 0])
-        drone_id = p.loadURDF(drone, basePosition=[0, 0, 0.7])
-
+        
     return total_nodes, success_percentage, total_time, time_per_iteration, nodes_per_iteration, success_percentage_per_iteration, path_lengths, rrt_graphs, nodes_per_ideal_path
    
+def statistics(nodes_per_ideal_path, num_iterations, total_nodes, success_percentage, total_time, time_per_iteration, nodes_per_iteration, success_percentage_per_iteration, path_lengths): 
+    # Compute statistics
+    if len(path_lengths) > 0:
+        mean_path_length = np.mean(path_lengths)
+        std_path_length = np.std(path_lengths)
+    else:
+        mean_path_length = 0.0
+        std_path_length = 0.0
+
+    mean_nodes = total_nodes / num_iterations # Wat zegt dit --> wss niet relevant
+    mean_success_percentage = (success_percentage / num_iterations) * 100
+    mean_total_time = total_time / num_iterations
+    std_nodes = np.std(nodes_per_iteration)
+    std_success_percentage = np.std(success_percentage_per_iteration) * 100
+    std_total_time = np.std(time_per_iteration)
+     # Calculate mean nodes per ideal path
+    mean_nodes_per_ideal_path = np.mean(nodes_per_ideal_path)
+    return mean_path_length, std_path_length, mean_nodes, mean_success_percentage, mean_total_time, std_nodes, std_success_percentage, std_total_time, mean_nodes_per_ideal_path
 
 # Create a function that runs the metrics function for different parameter sets
 def metrics(parameter_set=variable_parameters()):
@@ -103,7 +134,7 @@ def metrics(parameter_set=variable_parameters()):
     for key, value in parameter_set.items():
         step_size, max_iter, num_iterations = value
 
-        total_nodes, success_percentage, total_time, time_per_iteration, nodes_per_iteration, success_percentage_per_iteration, path_lengths, rrt_graphs, nodes_per_ideal_path = run_simulation_with_rrt_star(num_iterations, step_size, max_iter)
+        total_nodes, success_percentage, total_time, time_per_iteration, nodes_per_iteration, success_percentage_per_iteration, path_lengths, rrt_graphs, nodes_per_ideal_path = run_simulation_with_rrt_star(start, goal, obstacles, step_size, max_iter, num_iterations)
         mean_path_length, std_path_length, mean_nodes, mean_success_percentage, mean_total_time, std_nodes, std_success_percentage, std_total_time, mean_nodes_per_ideal_path = statistics(nodes_per_ideal_path, num_iterations, total_nodes, success_percentage, total_time, time_per_iteration, nodes_per_iteration, success_percentage_per_iteration, path_lengths)
 
         results[key] = {
@@ -130,8 +161,6 @@ for key, value in results.items():
     for metric, result in value.items():
         print(f"{metric}: {result}")
     print("\n")
-
-
 
 
 def plot_statistics(num_iterations, path_lengths, time_per_iteration, nodes_per_iteration, success_percentage_per_iteration, mean_path_length, std_path_length, mean_nodes, mean_success_percentage, mean_total_time):
